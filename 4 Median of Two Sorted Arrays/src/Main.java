c2976562da937227c9a7c7ad4501f5ba5880f99d
Mikkel <Mikkelnj411@gmail.com>
2021-09-11 23:51:33 +0200

    Median of Two Sorted Arrays

---
+++ 4 Median of Two Sorted Arrays/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+
+    Solution solution = new Solution();
+    int[] nums1 = new int[] {1,2};
+    int[] nums2 = new int[] {3,4};
+
+    System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+  }
+}
---
+++ 4 Median of Two Sorted Arrays/src/Solution.java
@@ -0,0 +1,47 @@
+public class Solution
+{
+  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+    int totalLength = nums1.length + nums2.length;
+    int[] mergedArray = new int[totalLength];
+
+    int counter = 0;
+    int i = 0;
+    int j = 0;
+
+    while (i+j < totalLength)
+    {
+      if (i == nums1.length){
+        while (j  < nums2.length)
+        {
+          mergedArray[counter++] = nums2[j++];
+        }
+        break;
+      }else if (j == nums2.length){
+        while (i  < nums1.length)
+        {
+          mergedArray[counter++] = nums1[i++];
+        }
+        break;
+      }
+
+
+      if (nums1[i] < nums2[j]){
+        mergedArray[counter++] = nums1[i++];
+      }else {
+        mergedArray[counter++] = nums2[j++];
+      }
+
+    }
+
+    double returnValue;
+
+    if (mergedArray.length % 2 == 0){
+
+      returnValue = (double) (mergedArray[mergedArray.length/2] + mergedArray[mergedArray.length/2 -1]) /2;
+    }else {
+      returnValue = mergedArray[mergedArray.length/2 ];
+    }
+    return returnValue;
+  }
+}
