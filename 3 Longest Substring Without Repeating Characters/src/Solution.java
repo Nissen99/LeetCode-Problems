2fcee362e31d61ddcc8e60d2b7ba944b4dc0f41e
Mikkel <Mikkelnj411@gmail.com>
2021-09-11 22:55:17 +0200

    length of substring

--- 3 Longest Substring Without Repeating Characters/src/Solution.java
+++ 3 Longest Substring Without Repeating Characters/src/Solution.java
@@ -1,3 +1,35 @@
-public class Solution
+class Solution
 {
+
+  public int lengthOfLongestSubstring(String s)
+  {
+
+    if (s.length() <= 1)
+      return s.length();
+
+    int biggest = 0;
+
+    StringBuilder building = new StringBuilder();
+
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (!building.toString().contains("" + s.charAt(i)))
+      {
+        building.append(s.charAt(i));
+
+        if (building.length() > biggest)
+        {
+          biggest = building.length();
+        }
+      }
+      else
+      {
+        building = new StringBuilder(
+            building.substring(building.toString().indexOf(s.charAt(i)) + 1));
+        building.append(s.charAt(i));
+      }
+    }
+    return biggest;
+  }
+
 }
