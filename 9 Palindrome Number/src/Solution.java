2aa462256a5f33f9aed7b577e6eed7900a90b667
Mikkel <Mikkelnj411@gmail.com>
2021-09-11 21:41:06 +0200

    LeetCode Problems

---
+++ 9 Palindrome Number/src/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+
+
+
+  public boolean isPalindrome(int x) {
+
+    if (x < 0){
+      return false;
+    }
+
+    String numberAsString = Integer.toString(x);
+
+    for (int i = 0; i < numberAsString.length(); i++)
+    {
+      if (numberAsString.charAt(i) == numberAsString.charAt(numberAsString.length() - 1 - i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
